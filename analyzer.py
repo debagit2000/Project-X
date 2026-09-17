97b1cd7ccb8019a4c380244889fa13f2734bd1bb
Deba <debashiskundu2026@gmail.com>
2026-09-17 16:49:34 +0530

    Fix model variable in analyze function

--- analyzer.py
+++ analyzer.py
@@ -2,7 +2,7 @@ import ollama
 from rag.retriever import search_docs
 def analyze(log_text):
  ctx=search_docs(log_text)
- r=ollama.chat(model='qwen2.5:3b',messages=[{'role':'user','content':f'Log:{log_text}
+ r=ollama.chat(model=MODEL,messages=[{'role':'user','content':f'Log:{log_text}
 Context:{ctx}
 Generate RCA and resolution'}])
  return r['message']['content']
